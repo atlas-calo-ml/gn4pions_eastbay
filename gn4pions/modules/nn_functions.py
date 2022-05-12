3f9b14255bac7dee7f1ff5a4ca7675b8fa0ed90a
Mariel Pettee <mariel.pettee@gmail.com>
2022-05-12 16:50:27 -0700

    Simple NN sanity checks to match EM and track resolution

--- gn4pions/modules/nn_functions.py
+++ gn4pions/modules/nn_functions.py
@@ -36,16 +36,18 @@ def load_data(file_path = "../data/onetrack_multicluster/pion_files/*.npy", n_fi
 
     ### Add track pT & truth particle E 
     track_pt = np.array(df.trackPt.explode())
-    truth_particle_e = np.array(df.truthPartE.explode())
     track_eta = np.array(df.trackEta.explode())
     track_phi = np.array(df.trackPhi.explode())
     track_z0 = np.array(df.trackZ0.explode())
+    truth_particle_e = np.array(df.truthPartE.explode())
+    truth_particle_pt = np.array(df.truthPartPt.explode())
 
     df2["track_pt"] = track_pt
     df2["track_eta"] = track_eta
     df2["track_phi"] = track_phi
     df2["track_z0"] = track_z0
     df2["truth_particle_e"] = truth_particle_e
+    df2["truth_particle_pt"] = truth_particle_pt
 
     ### Drop infs/NaNs 
     df2.replace([np.inf, -np.inf], np.nan, inplace=True)
@@ -90,19 +92,10 @@ def regression_model(train_x):
     model.compile(loss='mean_squared_error', optimizer='adam')
     return model
 
-def train(df, epochs=10): 
+def train(df, train_vars, target_var, epochs=10): 
     ### Test/train split 
     train = df.sample(frac=0.8, random_state=0)
     test = df.drop(train.index)
-
-    train_vars = [
-        'log10_cluster_e_0', 
-    #     'log10_track_pt',
-    #     'track_eta'
-                 ]
-
-    # target_var = 'log10_truth_particle_e'
-    target_var = 'log10_cluster_calib_e_0'
 
     train_x = train[train_vars].values
     train_y = train[target_var].values
@@ -126,12 +119,14 @@ def train(df, epochs=10):
     test['nn_output'] = model.predict(test_x)
     return(test)
 
-def make_plots(test, save_label=None): 
+def make_plots(test, plot_em = False, plot_track = False, save_label=None): 
     pred   = test.nn_output
     target = test.nn_target
     x = 10**target
-    y = pred/target
+    y = 10**pred/10**target
+    
     y_em = test.cluster_e_0/x
+    y_track = test.track_pt*np.cosh(test.track_eta)/x#(test.truth_particle_pt*np.cosh(test.track_eta))
     
 #     plt.hist(y, bins=np.linspace(0.9,1.1,40));
     
@@ -142,6 +137,7 @@ def make_plots(test, save_label=None):
     
     median_em = stats.binned_statistic(x, y_em, bins=xbin, statistic='median').statistic
     median_nn = stats.binned_statistic(x, y, bins=xbin, statistic='median').statistic
+    median_track = stats.binned_statistic(x, y_track, bins=xbin, statistic='median').statistic
     
     c_map = ListedColormap(sns.color_palette("Blues", n_colors=100).as_hex())
     #     plt.hist2d(x, y, bins=[xbin, ybin], norm=LogNorm(),zorder = -1, cmap=c_map);
@@ -150,19 +146,23 @@ def make_plots(test, save_label=None):
     fig.patch.set_facecolor('white')
 
     ax = axs[0]
-    ax.plot(np.array(xcenter), np.array(median_em), color='indianred', linewidth=3, label="EM")
     ax.plot(np.array(xcenter), np.array(median_nn), color='teal', linewidth=3, label="NN")
+    if plot_em: 
+        ax.plot(np.array(xcenter), np.array(median_em), color='indianred', linewidth=3, label="EM")
+    if plot_track:
+        ax.plot(np.array(xcenter), np.array(median_track), color='violet', linewidth=3, label="Track")
     ax.plot([0.1, 1000], [1, 1], linestyle='--', color='black');
     ax.set_xscale('log')
-    ax.set_ylim(0, 1.75)
+    ax.set_ylim(0.9, 1.2)
     ax.set_xlim(0.3, )
     ax.tick_params(axis='x', labelsize=15)
     ax.tick_params(axis='y', labelsize=15)
-    ax.set_xlabel('True Cluster Energy [GeV]', fontsize=15)
+    ax.set_xlabel('True Particle Energy [GeV]', fontsize=15)
     ax.set_ylabel('Predicted Energy / Target', fontsize=15);
     ax.set_title('Response Median', fontsize=20)
     ax.legend(fontsize=15)
-    np.savez('pub_note_results/response_medians_{}.npz'.format(save_label), response_median=median_nn, xcenter=xcenter)
+    if save_label is not None:
+        np.savez('pub_note_results/response_medians_{}.npz'.format(save_label), response_median=median_nn, xcenter=xcenter)
     
     ### IQR plot 
     def iqrOverMed(x):
@@ -175,17 +175,22 @@ def make_plots(test, save_label=None):
 
     iqr_em = stats.binned_statistic(x, y_em, bins=xbin, statistic=iqrOverMed).statistic
     iqr_nn = stats.binned_statistic(x, y, bins=xbin, statistic=iqrOverMed).statistic
-
+    iqr_track = stats.binned_statistic(x, y_track, bins=xbin, statistic=iqrOverMed).statistic
+    
     ax = axs[1]
-    ax.plot(xcenter, iqr_em, linewidth=3, color='indianred', label="EM")
     ax.plot(xcenter, iqr_nn, linewidth=3, color='teal', label="NN")
+    if plot_em: 
+        ax.plot(xcenter, iqr_em, linewidth=3, color='indianred', label="EM")
+    if plot_track:
+        ax.plot(xcenter, iqr_track, linewidth=3, color='violet', label="Track Resolution")
     ax.set_xscale('log')
-    ax.set_ylim(0, 1.75)
+    ax.set_ylim(0, 0.5)
     ax.set_xlim(0.3, )
     ax.tick_params(axis='x', labelsize=15)
     ax.tick_params(axis='y', labelsize=15)
-    ax.set_xlabel('True Cluster Energy [GeV]', fontsize=15)
+    ax.set_xlabel('True Particle Energy [GeV]', fontsize=15)
     ax.set_ylabel('Response IQR / (2 x Median)', fontsize=15);
     ax.set_title('IQR', fontsize=20)
     ax.legend(fontsize=15)
-    np.savez('pub_note_results/iqr_{}.npz'.format(save_label), iqr=iqr_nn, xcenter=xcenter)
+    if save_label is not None:
+        np.savez('pub_note_results/iqr_{}.npz'.format(save_label), iqr=iqr_nn, xcenter=xcenter)
